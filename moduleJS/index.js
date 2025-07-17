873a65053a440b2ba293c043ae42d8e7fd2ccb2f
SHUBBHAM-KUMARR <shubbhammkumarr@outlook.com>
2025-07-17 14:17:48 +0530

    project added

---
+++ moduleJS/index.js
@@ -0,0 +1,56 @@
+import {tags }from './utils.js'
+
+ const mainDiv = tags('div')
+
+mainDiv.setAttribute("id", "mainContainer");
+document.body.appendChild(mainDiv);
+
+
+ let header =tags('div');
+mainDiv.append(header);
+header.setAttribute("id", "header")
+
+let cNameDiv = tags('div')
+header.append(cNameDiv)
+cNameDiv.setAttribute("id","cNameDiv")
+
+let cName = tags('h1')
+cNameDiv.append(cName)
+cName.innerText = "StepUPseed"
+
+
+let searchDiv = tags('div')
+searchDiv.setAttribute('id','searchDiv')
+header.append(searchDiv)
+
+let searchInput = tags('input')
+searchInput.setAttribute('type','search')
+searchInput.setAttribute('id','searchInput')
+searchInput.setAttribute('placeholder','searchInput')
+searchDiv.append(searchInput)
+
+let searchIconDiv = tags('div')
+searchIconDiv.setAttribute('id','srchIconDiv')
+searchDiv.append(searchIconDiv)
+
+let searchIcon = tags('img');
+searchIcon.setAttribute('id','searchIcon')
+searchIcon.setAttribute('src','./media/searchblack.png')
+searchIconDiv.append(searchIcon)
+
+let nav = tags('nav')
+header.append(nav)
+nav.setAttribute('id','nav')
+
+let ul = tags('ul')
+nav.append(ul)
+ul.setAttribute('id','nav_ul');
+
+const siteLinks = tags('li', 4);
+const linkTexts = ['Services', 'Products', 'About ','Contact '];
+
+siteLinks.forEach((li,i) => {
+  li.textContent = linkTexts[i];
+  ul.appendChild(li);
+  li.setAttribute('class','sites')
+});
---
+++ moduleJS/utils.js
@@ -0,0 +1,26 @@
+
+
+export let tags = (tag, num = 1) => {
+  const htmlTags = [
+    'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+    'p', 'span', 'strong', 'em', 'br', 'blockquote', 'pre',
+    'div', 'header', 'footer', 'main', 'section', 'article', 'aside', 'nav',
+    'ul', 'ol', 'li', 'dl', 'dt', 'dd',
+    'table', 'thead', 'tbody', 'tfoot', 'tr', 'td', 'th', 'caption', 'colgroup', 'col',
+    'form', 'input', 'textarea', 'button', 'select', 'option', 'label', 'fieldset', 'legend',
+    'img', 'audio', 'video', 'source', 'track', 'canvas', 'svg', 'iframe',
+    'a', 'link', 'meta', 'title', 'style', 'script', 'base',
+    'details', 'summary', 'dialog', 'menu', 'menuitem',
+    'figure', 'figcaption', 'mark', 'time', 'code', 'kbd', 'samp', 'var', 'abbr', 'cite', 'dfn'
+  ];
+
+  if (!htmlTags.includes(tag)) {
+    throw new Error("Invalid HTML tag");
+  }
+
+  if (num === 1) {
+    return document.createElement(tag);
+  } else {
+    return Array.from({ length: num }, () => document.createElement(tag));
+  }
+};
